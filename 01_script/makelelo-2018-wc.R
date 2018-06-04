e83ad3607152047575aa408e81b512eea89d6b17
Fooourche <fabien.rinaldi@gmail.com>
2018-06-04 21:53:13 +0200

    add files

--- 01_script/makelelo-2018-wc.R
+++ 01_script/makelelo-2018-wc.R
@@ -10,6 +10,7 @@ library(lubridate)
 library(tidyverse)
 library(h2o)
 library(timetk)
+library(tidyquant)
 
 # Read in the world cup CSV data
 training = read_csv("00_data/wc_datathon_dataset.csv")
@@ -35,6 +36,88 @@ elo_run = elo.run(
 
 
 training <- cbind(training,elo_run)
+
+training <- training %>% 
+              mutate(result=case_when((team_1_goals-team_2_goals) == 0 ~ 0,
+                                      (team_1_goals-team_2_goals) < 0 ~ 2,
+                                      (team_1_goals-team_2_goals) > 0 ~ 1))
+
+
+training_aug <- training %>%
+                tk_augment_timeseries_signature()
+
+
+
+training_clean <- training_aug %>%
+                    select_if(~ !is.Date(.)) %>%
+                    select_if(~ !any(is.na(.))) %>%
+                    mutate_if(is.ordered, ~ as.character(.) %>% as.factor)
+
+training_clean %>% glimpse()
+
+
+train_tbl <- training_clean %>% filter(year < 2014)
+valid_tbl <- training_clean %>% filter(year >= 2014 & year <= 2016)
+test_tbl  <- training_clean %>% filter(year > 2016)
+
+h2o.init()
+
+
+train_h2o <- as.h2o(train_tbl)
+valid_h2o <- as.h2o(valid_tbl)
+test_h2o  <- as.h2o(test_tbl)
+
+
+# Set names for h2o
+y <- "result"
+x <- setdiff(names(train_h2o), y)
+
+
+# linear regression model used, but can use any model
+automl_models_h2o <- h2o.automl(
+                        x = x, 
+                        y = y, 
+                        training_frame = train_h2o, 
+                        validation_frame = valid_h2o, 
+                        leaderboard_frame = test_h2o, 
+                        max_runtime_secs = 60, 
+                        stopping_metric = "deviance")
+
+# Extract leader model
+automl_leader <- automl_models_h2o@leader
+
+
+pred_h2o <- h2o.predict(automl_leader, newdata = test_h2o)
+
+
+h2o.performance(automl_leader, newdata = test_h2o)
+
+# Investigate test error
+error_tbl <- training %>% 
+  filter(lubridate::year(date) == 2017) %>%
+  add_column(pred = pred_h2o %>% as.tibble() %>% pull(predict)) %>%
+  rename(actual = result) %>%
+  mutate(
+    error     = actual - pred,
+    error_pct = error / actual
+  ) 
+error_tbl
+
+
+error_tbl %>%
+  summarise(
+    me   = mean(error),
+    rmse = mean(error^2)^0.5,
+    mae  = mean(abs(error)),
+    mape = mean(abs(error_pct)),
+    mpe  = mean(error_pct)
+  ) %>%
+  glimpse()
+
+
+
+
+
 
 # Draw Rates
 draw_rates = data.frame(win_prob = elo_run$elos[,3],win_loss_draw = elo_run$elos[,4]) %>%
